88db63d22e4d34b61bf381aa522930925b4ea550
nichjang <nich.jang@gmail.com>
2018-02-20 18:09:57 +0100

    batch least squares

--- rbf.py
+++ rbf.py
@@ -22,11 +22,26 @@ def least_squares(data, values, N = None):
     w = np.linalg.solve(phi, values)
     return w, rbfs
 
+def iterative_batch(train_data, values, error=0.1, N = None):
+    if N == None:
+        N = len(train_data)
+    w = np.random.normal(-1, 1, N)
+    mu = np.random.choice(train_data, N)
+    sig = [min(a - b for b in train_data if a != b)**2 for a in train_data]
+    rbfs = [RBF(m, s) for m, s in zip(mu, sig)]
+    big_phi = np.array([[rbf.phi(x) for rbf in rbfs] for x in train_data ])
+    total_error =  sum((big_phi.dot(w) - values) ** 2)
+
+    w = np.linalg.lstsq(big_phi.T.dot(big_phi), big_phi.T.dot(values), rcond=-1)[0]
+    total_error =  sum((big_phi.dot(w) - values) ** 2)
+    return w, rbfs
+
 if __name__ == "__main__":
-    train_data = list(np.arange(0, 2*np.pi, .5))
-    test_data = list(np.arange(-1, 3*np.pi, .1))
+    train_data = list(np.arange(0, 2*np.pi, .1))
+    test_data = list(np.arange(0.05, 2*np.pi, .1))
     values = np.array([np.sin(x) for x in train_data])
-    w, rbfs = least_squares(train_data, values)
+    w, rbfs = iterative_batch(train_data, values, N=5)
+    # w, rbfs = least_squares(train_data, values)
     result = [run(w, rbfs, x) for x in test_data]
     plot.plot(train_data, values, [-1, 1])
     plot.plot(test_data, result, [-1, 1])
